a82174e637557797353c945adbcc7390a4b448d4
Henrique Kraemer <contato@henriquekraemer.com>
2015-02-09 19:00:27 -0200

    Improved description/image sample Enable Dither

--- Dither-Enable/enable_dither.java
+++ Dither-Enable/enable_dither.java
@@ -1,2 +1,17 @@
+﻿/*
+*
+*	Sample: Enable Dither
+* 		by Henrique Kraemer (www.henriquekraemer.com)
+*							(www.github.com/henriquekraemer)
+*
+*
+*	NOTES:
+*	By default, the dither is disabled in Ethanon (android). Which may cause 
+*	the color banding (http://en.wikipedia.org/wiki/Colour_banding).
+*	
+*
+*/
+
+
 /* Add this line in the onCreate method of EthanonActivity */
 GS2DJNI.setSharedData("ethanon.system.gles2dither", "enable");
