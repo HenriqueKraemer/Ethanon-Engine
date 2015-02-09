1f41e1f01cf00117e6c99e382956335d2e1a5ec2
Henrique Kraemer <contato@henriquekraemer.com>
2015-02-09 19:06:07 -0200

    Update enable_dither.java

--- Dither-Enable/enable_dither.java
+++ Dither-Enable/enable_dither.java
@@ -13,5 +13,5 @@
 */
 
 
-/* Add this line in the onCreate method of EthanonActivity */
+/* Add the line below in the onCreate method of EthanonActivity */
 GS2DJNI.setSharedData("ethanon.system.gles2dither", "enable");
